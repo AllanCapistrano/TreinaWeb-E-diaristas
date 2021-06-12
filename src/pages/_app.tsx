4b97fef1b6d45771c9700ecfccb15a29d3e605c4
AllanCapistrano <allan.capistrano3@gmail.com>
2021-06-11 22:30:57 -0300

    Second class.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from '@material-ui/core';
 
 import theme from 'ui/themes/theme';
 import Header from 'ui/components/surfaces/Header/Header';
+import Footer from 'ui/components/surfaces/Footer/Footer';
+import { AppContainer } from 'ui/styles/pages/_app.style';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -21,8 +23,11 @@ function MyApp({ Component, pageProps }) {
         />
       </Head>
       <ThemeProvider theme={theme}>
-        <Header />
-        <Component {...pageProps} />
+        <AppContainer>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </AppContainer>
       </ThemeProvider>
     </>
   );
--- src/ui/components/surfaces/Footer/Footer.style.tsx
+++ src/ui/components/surfaces/Footer/Footer.style.tsx
@@ -1,1 +1,43 @@
 import { styled } from '@material-ui/core/styles';
+import { Container, Typography } from '@material-ui/core';
+
+export const FooterStyled = styled('footer')`
+    background-color: ${({ theme }) => theme.palette.primary.main};
+    
+    /*A função getContrastText() encontra uma cor de contraste a
+    partir da cor passada como parâmetro*/
+    color: ${({ theme }) => 
+    theme.palette.getContrastText(theme.palette.primary.main)};
+    padding: ${({ theme }) => theme.spacing(4)} 0;
+    margin-top: auto;
+`;
+
+export const FooterContainer = styled(Container)`
+    display: flex;
+    justify-content: space-between;
+    gap: ${({ theme }) => theme.spacing(2)};
+    flex-wrap: wrap; //Quando não tiver espaço, coloca os elementos em coluna
+
+    ${({ theme }) => theme.breakpoints.down('md')} {
+        gap: ${({ theme }) => theme.spacing(5)};
+    };
+`;
+
+export const FooterTitle = styled((props) => (
+    //Estilizando um componente que será retornado, para não
+    //precisar passar essas propriedades toda vez que for utilizá-lo
+    <Typography component={'h2'} variant={'body2'} {...props} />
+))`
+    font-weight: bold;
+`;
+
+export const AppList = styled('ul')`
+    display: flex;
+    list-style-type: none;
+    padding: 0;
+    gap: ${({ theme }) => theme.spacing()};
+
+    img {
+        width: 122px;
+    }
+`;
